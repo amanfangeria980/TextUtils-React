370e66e0310eeb6d92cdaf7415585c8e634cd310
amanfangeria980 <amanfangeria980@gmail.com>
2022-05-23 11:28:12 +0530

    added some more logic like text summary and did some colour modification to our navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,24 +4,24 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{props.title}</a>
+    <a className="navbar-brand text-light" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+          <a className="nav-link text-light" href="/">Link</a>
         </li>
       </ul>
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-primary" type="submit">Search</button>
       </form>
     </div>
   </div>
--- src/components/textform.js
+++ src/components/textform.js
@@ -9,25 +9,43 @@ export default function Textform(props) {
     setText(newText);
     console.log("After: ",text);
   }
+  const handleLoClick =()=>{
+    let newText=text.toLowerCase();
+    setText(newText);
+  }
   const handleOnChange=(event)=>{
     console.log("You tried to change the text");
     setText(event.target.value);
   }
   // const[count,setCount]=useState(0);
-  const[text,setText]=useState("Enter Text Here");
+  const[text,setText]=useState("");
   // text="new text" //wrong way to change the text
   // setText("new text"); //correct way to change the text
   return (
-  <div>
+  <>
+  <div className='container'>
     <h1>{props.heading}</h1>
     <div className="mb-3">
     {/* here form field will be rendered as read only field without an Onchange handler as we have provided a 'value' prop. To solve this Otherwise, set either `onChange` or `readOnly` */}
     {/* <textarea className="form-control" id="myBox" rows="3" value={text}></textarea> */}
     {/* here now onchange handler is used so it will not provide any error in out console. */}
     <textarea className="form-control" id="myBox" rows="3" value={text} onChange={handleOnChange}></textarea>
-
     </div>
-    <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
   </div>
+
+  <div className="container my-2">
+    <h2>Your Text Summary</h2>
+    {/* to see how many words and characters in textbox */}
+    <p>{text.split(" ").length} words and {text.length} characters</p>
+    {/* to calculate and show the amount of time required to read or total read time word by word by a user */}
+    <p>{0.008 *text.split(" ").length} minuted read by you</p>
+    {/* to see the preview */}
+    <h2>Preview</h2>
+    <p>{text}</p>
+    
+  </div>
+  </>
   )
 }
